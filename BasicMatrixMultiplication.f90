4de0804e02aa05e1d44114defc68437485bb32ba
Deepak Aggarwal <deepakagg@ipr.res.in>
2023-08-25 17:29:57 +0530

    Basic matrix multiplication file without any external library added

---
+++ BasicMatrixMultiplication.f90
@@ -0,0 +1,42 @@
+program BasicMatrixMultiplication
+    implicit none
+
+    integer, parameter :: N = 1000
+    integer, parameter :: M = 1000
+    integer, parameter :: K = 1000
+
+    real(8), dimension(N, K) :: A
+    real(8), dimension(K, M) :: B
+    real(8), dimension(N, M) :: C
+
+    integer :: i, j, q
+    real(8) :: start_time, end_time  ! Declare the timing var
+
+    ! Initialize matrices A and B with random values
+    do j = 1, K
+        do i = 1, N
+            A(i, j) = rand()
+        end do
+    end do
+
+    do j = 1, M
+        do i = 1, K
+            B(i, j) = rand()
+        end do
+    end do
+
+    call cpu_time(start_time)
+    ! Perform matrix multiplication
+    do j = 1, M
+        do i = 1, N
+            C(i, j) = 0.0
+            do q = 1, K
+                C(i, j) = C(i, j) + A(i, q) * B(q, j)
+            end do
+        end do
+    end do
+    call cpu_time(end_time)
+
+    print *, "Time without ext library in seconds:", end_time - start_time
+
+end program BasicMatrixMultiplication
